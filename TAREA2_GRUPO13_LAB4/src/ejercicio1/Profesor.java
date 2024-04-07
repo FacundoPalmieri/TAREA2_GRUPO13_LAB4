65382083a526baf21fc7be345a129086fac92ff2
Karina Rivera <karina.rivera@hotmail.es>
2024-04-07 12:01:56 -0300

    agrego constructores

--- TAREA2_GRUPO13_LAB4/src/ejercicio1/Profesor.java
+++ TAREA2_GRUPO13_LAB4/src/ejercicio1/Profesor.java
@@ -6,3 +6,15 @@ public class Profesor extends Empleado {
 	private int antiguedadDocente;
 
 }
+
+public Profesor() {
+	super();
+	cargo= "sin nombre";
+	antiguedadDocente=0;
+}
+
+public Profesor(String Nombre, int Edad, String Cargo, int Antiguedad) {
+	super (Nombre, Edad);
+	cargo= Cargo;
+	antiguedadDocente= Antiguedad;
+}
