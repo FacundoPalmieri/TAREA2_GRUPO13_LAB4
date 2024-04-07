a1e7e7e3997b8d8ffa880c8261f195df9dbe1742
Karina Rivera <karina.rivera@hotmail.es>
2024-04-07 13:15:46 -0300

    corrigo clase profesor

--- TAREA2_GRUPO13_LAB4/src/ejercicio1/Profesor.java
+++ TAREA2_GRUPO13_LAB4/src/ejercicio1/Profesor.java
@@ -5,16 +5,18 @@ public class Profesor extends Empleado {
 	private String cargo;
 	private int antiguedadDocente;
 
+
+	public Profesor() {
+		super();
+		cargo= "sin nombre";
+		antiguedadDocente=0;
+	}
+	
+	public Profesor(String Nombre, int Edad, String Cargo, int Antiguedad) {
+		super (Nombre, Edad);
+		cargo= Cargo;
+		antiguedadDocente= Antiguedad;
+	}
+
 }
 
-public Profesor() {
-	super();
-	cargo= "sin nombre";
-	antiguedadDocente=0;
-}
-
-public Profesor(String Nombre, int Edad, String Cargo, int Antiguedad) {
-	super (Nombre, Edad);
-	cargo= Cargo;
-	antiguedadDocente= Antiguedad;
-}
