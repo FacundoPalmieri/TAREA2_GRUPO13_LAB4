a9df799648de79cba56ae86b56625b69605bb6e4
unknown <palmierifacundo@gmail.com>
2024-04-09 20:02:37 -0300

    Metodo ToString + clase Main

---
+++ TAREA2_GRUPO13_LAB4/src/ejercicio1/MainEjercicio1_a.java
@@ -0,0 +1,35 @@
+package ejercicio1;
+
+import java.util.ArrayList;
+
+public class MainEjercicio1_a {
+
+	public static void main(String[] args) {
+		
+		Profesor p1 = new Profesor("Juan",40,"Titular", 10);
+	    Profesor p2 = new Profesor("María",35,"Titular", 8);
+	    Profesor p3 = new Profesor("Carlos",50,"Ayudante", 15);
+	    Profesor p4 = new Profesor("Ana", 45,"Titular", 12);
+	    Profesor p5 = new Profesor("Pedro", 55,"Suplente", 20);
+		
+		ArrayList<Profesor> listaProfesores = new ArrayList<Profesor>(5);
+		listaProfesores.add(p1);
+		listaProfesores.add(p2);
+		listaProfesores.add(p3);
+		listaProfesores.add(p4);
+		listaProfesores.add(p5);
+	
+		System.out.println("Listado de Profesores: ");
+
+		for(int i=0; i<listaProfesores.size(); i++)
+		{
+			System.out.println(listaProfesores.get(i).toString());
+		}
+		
+	
+		
+		
+		
+	}
+
+}
--- TAREA2_GRUPO13_LAB4/src/ejercicio1/Profesor.java
+++ TAREA2_GRUPO13_LAB4/src/ejercicio1/Profesor.java
@@ -5,16 +5,25 @@ public class Profesor extends Empleado {
 	private String cargo;
 	private int antiguedadDocente;
 
-}
 
-public Profesor() {
-	super();
-	cargo= "sin nombre";
-	antiguedadDocente=0;
-}
+	public Profesor() {
+		super();
+		cargo= "sin nombre";
+		antiguedadDocente=0;
+	}
+	
+	public Profesor(String Nombre, int Edad, String Cargo, int Antiguedad) {
+		super (Nombre, Edad);
+		cargo= Cargo;
+		antiguedadDocente= Antiguedad;
+	}
 
-public Profesor(String Nombre, int Edad, String Cargo, int Antiguedad) {
-	super (Nombre, Edad);
-	cargo= Cargo;
-	antiguedadDocente= Antiguedad;
+	@Override
+	public String toString() {
+		return "Id: " + getId() + " - Nombre: " + getNombre() + " -Edad: " + getEdad() +
+			   " - Cargo: " + cargo + " - AntiguedadDocente: " + antiguedadDocente;
+	}
+
+
+
 }
