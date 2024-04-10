4129b00a5e570323fefe316fade40398fcb0fc22
Karina Rivera <karina.rivera@hotmail.es>
2024-04-10 12:03:06 -0300

    modifico clase productos refrigerados

--- TAREA2_GRUPO13_LAB4/src/ejercicio2/ProductosRefrigerados.java
+++ TAREA2_GRUPO13_LAB4/src/ejercicio2/ProductosRefrigerados.java
@@ -1,8 +1,23 @@
 package ejercicio2;
 
 public class ProductosRefrigerados extends Producto {
-private int temperaturaCongelacionRecomendada;
+	private int temperaturaCongelacionRecomendada;
+	private String codigoOrganismoSupervisor;
 	
+	public String getCodigoOrganismoSupervisor() {
+		return codigoOrganismoSupervisor;
+	}
+
+	public void setCodigoOrganismoSupervisor(String codigoOrganismoSupervisor) {
+		this.codigoOrganismoSupervisor = codigoOrganismoSupervisor;
+	}
+
+	@Override
+	public String toString() {
+		return "ProductosRefrigerados [temperaturaCongelacionRecomendada=" + temperaturaCongelacionRecomendada
+				+ ", codigoOrganismoSupervisor=" + codigoOrganismoSupervisor + "]";
+	}
+
 	//Constructors
 	public ProductosRefrigerados() {
 		
@@ -30,10 +45,5 @@ private int temperaturaCongelacionRecomendada;
 	}
 
 
-	//ToString		
-	@Override
-	public String toString() {
-		return "Producto Refrigerado: Temperatura recomendada:" + temperaturaCongelacionRecomendada + ", " + super.toString();
-	}
 	
 }
